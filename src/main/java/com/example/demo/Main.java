4f2adb67028ba86c6b170ed5b7159cd6e196365d
necoskijanen <100243987+necoskijanen@users.noreply.github.com>
2025-10-17 07:56:15 +0900

    add start,finish message

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -2,11 +2,28 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.EventListener;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class Main {
 
     public static void main(String[] args) {
+        log.info("アプリケーションを起動します");
         SpringApplication.run(Main.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady() {
+        log.info("アプリケーションが正常に起動しました");
+    }
+
+    @EventListener(ContextClosedEvent.class)
+    public void onApplicationShutdwn() {
+        log.info("アプリケーションを終了します");
     }
 }
